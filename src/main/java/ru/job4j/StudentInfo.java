8d62c2e8f5854ad45b87dc17fd361b21f5a362c7
LogOS0408 <bsv-81@mail.ru>
2024-06-09 20:43:27 +0400

    4.2. Результат работы метода [#232622 #316177]

--- src/main/java/ru/job4j/StudentInfo.java
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -1,8 +1,8 @@
 package ru.job4j;
 
 public class StudentInfo {
-    public static void main (String[] args){
-        System.out.println("Stanislav Batuev.");
+    public static void main(String[] args) {
+        System.out.println("Stanislav Batuev");
         System.out.println("20.01.1981");
     }
 }
