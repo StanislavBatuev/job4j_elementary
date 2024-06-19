b38050add4c09cdf41c08b4b187adf2123880903
LogOS0408 <bsv-81@mail.ru>
2024-06-19 16:15:36 +0400

    1.1. Что такое import [#504773 #316219]

---
+++ src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,28 @@
+package ru.job4j.calculator;
+
+import static ru.job4j.math.MathFunction.*;
+
+public class MathCalculator {
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double divisionAndSubstraction(double first, double second) {
+        return division(first, second)
+                + subtraction(first, second);
+    }
+
+    public static double quadrupleAction(double first, double second) {
+        return division(first, second)
+                + subtraction(first, second)
+                + sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчёта равен - " + sumAndMultiply(40, 20)
+        + "\n" + divisionAndSubstraction(20, 5)
+        + quadrupleAction(77, 55));
+    }
+}
