8afd5848b40031209176388b83050cb14c3ccfed
LogOS0408 <bsv-81@mail.ru>
2024-06-25 15:04:59 +0400

    5.9. Крест в псевдографике.  [#177182 #325330]

---
+++ src/main/java/ru/job4j/loop/Slash.java
@@ -0,0 +1,21 @@
+package ru.job4j.loop;
+
+public class Slash {
+    public static void draw(int size) {
+        for (int row = 0; row < size; row++) {
+            for (int cell = 0; cell < size; cell++) {
+                if (row == cell || cell == size - 1 - row) {
+                    System.out.print("0");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        draw(5);
+    }
+}
