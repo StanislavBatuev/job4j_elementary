1c4e537f493c1d4868c92b55ece9c6ae37aa296f
LogOS0408 <bsv-81@mail.ru>
2024-06-26 08:56:46 +0400

    6.0.2. Размер массива [#333582 #326090]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,7 +1,13 @@
 package ru.job4j.array;
 
 public class ArrayDefinition {
-    short[] ages = new short[10];
-    String[] surnames = new String[100500];
-    float[] prices = new float[40];
+    public static void main(String[] args) {
+        short[] ages = new short[10];
+        String[] surnames = new String[100500];
+        float[] prices = new float[40];
+        System.out.println("Размер массива ages - " + ages.length + "\n"
+                + "Размер массива surnames - " + surnames.length + "\n"
+                + "Размер массива prices - " + prices.length);
+
+    }
 }
