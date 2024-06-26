076846bce098dcc031c748139bc284c8c73919ca
LogOS0408 <bsv-81@mail.ru>
2024-06-26 09:24:32 +0400

    6.0.3. Заполнение массива. [#226300 #326099]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -2,12 +2,18 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
+        String[] surnames;
         short[] ages = new short[10];
-        String[] surnames = new String[100500];
+        surnames = new String[100500];
         float[] prices = new float[40];
         System.out.println("Размер массива ages - " + ages.length + "\n"
                 + "Размер массива surnames - " + surnames.length + "\n"
                 + "Размер массива prices - " + prices.length);
-
+        String[] names = new String[4];
+        names[0] = "Peiya";
+        names[1] = "Vasya";
+        names[2] = "Kolya";
+        names[3] = "Doryna";
+        System.out.println(names[3]);
     }
 }
