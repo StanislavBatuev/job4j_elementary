3284194a195021323843efe8a89d12b2cd251ee0
LogOS0408 <bsv-81@mail.ru>
2024-06-28 14:29:23 +0400

    6.5. Поиск индекса в диапазоне. [#156318 #330022]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -11,4 +11,26 @@ public class FindLoop {
         }
         return result;
     }
+
+    public static int indexInRange(int[] data, int element, int start, int finish) {
+        int result = -1;
+        if (start > finish || data.length < start || data.length < finish || data.length == 0) {
+            return -1;
+        }
+
+        for (int i = 0; i < data.length; i++) {
+            if (i >= start && i <= finish) {
+                if (data[i] == element) {
+                    result = i;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] array = {5, 10, 2, 4, 8, 4, 14, 4, 3, 21, 16};
+        System.out.println(indexInRange(array, 4, 1, 8));
+    }
 }
