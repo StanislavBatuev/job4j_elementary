5e904e9d6451970560c06c768d4b56942b85b8c9
LogOS0408 <bsv-81@mail.ru>
2024-06-29 16:33:02 +0400

    6.6.0. Двумерный массив [#226796 #330967]

--- src/main/java/ru/job4j/array/Sequence.java
+++ src/main/java/ru/job4j/array/Sequence.java
@@ -6,11 +6,14 @@ public class Sequence {
         array[0][0] = 1;
         array[0][1] = 2;
         array[0][2] = 3;
-        array[1][1] = 4;
-        array[1][2] = 5;
-        array[1][3] = 6;
-        array[2][1] = 7;
-        array[2][2] = 8;
-        array[2][3] = 9;
+        array[1][0] = 4;
+        array[1][1] = 5;
+        array[1][2] = 6;
+        array[2][0] = 7;
+        array[2][1] = 8;
+        array[2][2] = 9;
+        for (int[] ints : array) {
+            System.out.println("Размер вложенного массива равен: " + ints.length);
+        }
     }
 }
