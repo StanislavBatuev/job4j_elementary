db823633da1cb156b75b956ab3109f56b894f331
LogOS0408 <bsv-81@mail.ru>
2024-07-01 09:03:17 +0400

    6.7.4. Выигрышные комбинации в сокобан [#53859 #331821]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -31,4 +31,17 @@ public class MatrixCheck {
         }
         return result;
     }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 'X' && MatrixCheck.monoVertical(board, i)
+                    || MatrixCheck.monoHorizontal(board, i)) {
+                result = true;
+                break;
+            }
+
+        }
+        return result;
+    }
 }
