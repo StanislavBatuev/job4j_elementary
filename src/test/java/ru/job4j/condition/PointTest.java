1f594cd3eefd01e705508bdd48809e4ca13ea555
LogOS0408 <bsv-81@mail.ru>
2024-07-09 18:19:05 +0400

    2.0. Рефакторинг - Расстояние между точками. [#122587 #340715]

--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,25 +8,28 @@ class PointTest {
 
     @Test
     void when771010Then4Dot2426() {
-        int x1 = 7, y1 = 7, x2 = 10, y2 = 10;
-        double expected = 4.2426;
-        double output = Point.distance(x1, y1, x2, y2);
-        assertThat(output).isEqualTo(expected, withPrecision(0.0001));
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double expected = 2;
+        double output = a.distance(b);
+        assertThat(output).isEqualTo(expected);
     }
 
     @Test
     void when7Minus7Minus10Minus10Then17Dot2626() {
-        int x1 = 7, y1 = -7, x2 = -10, y2 = -10;
+        Point a = new Point(-10, -10);
+        Point b = new Point(7, -7);
         double expected = 17.2626;
-        double output = Point.distance(x1, y1, x2, y2);
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.0001));
     }
 
     @Test
     void when7710Minus100Then107Dot0420() {
-        int x1 = 7, y1 = 7, x2 = 10, y2 = -100;
-        double expected = 107.0420;
-        double output = Point.distance(x1, y1, x2, y2);
+        Point a = new Point(10, -100);
+        Point b = new Point(7, -7);
+        double expected = 93.0483;
+        double output = a.distance(b);
         assertThat(output).isEqualTo(expected, withPrecision(0.0001));
     }
 }
