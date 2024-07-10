1987ac9036b3be418ce02b26e40d53ba56d844f4
LogOS0408 <bsv-81@mail.ru>
2024-07-10 14:03:58 +0400

    3. Перегрузить метод max для трех чисел [#122644 #342099]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -5,7 +5,24 @@ public class Max {
         return first > second ? first : second;
     }
 
+    public static int max(int first, int second, int third, int forth) {
+        return Math.max(Math.max(first, second), Math.max(third, forth));
+    }
+
+    public static int max(int first, int second, int third) {
+        return Math.max(Math.max(first, second), third);
+    }
+
+    public static double max(double first, int second, int third) {
+        return Math.max(Math.max(first, second), third);
+    }
+
+    public static double max(int first, double second, int third) {
+        return Math.max(Math.max(first, second), third);
+    }
+
     public static void main(String[] args) {
         System.out.println(comparisonOfNumbers(500, 355));
+        System.out.println(max(555.55, 500, 355));
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -13,4 +13,38 @@ class MaxTest {
         int output = Max.comparisonOfNumbers(first, second);
         assertThat(output).isEqualTo(expected);
     }
+
+    @Test
+    void whenFirst500Second355Third444Forth777Then777() {
+        int second = 355, first = 500, third = 444, forth = 777;
+        int expected = 777;
+        int output = Max.max(first, second, third, forth);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenFirst500Second355Third444hen500() {
+        int second = 355, first = 500, third = 444;
+        int expected = 500;
+        int output = Max.max(first, second, third);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenFirst11Dot11Second10Third5Then11Dot11() {
+        int second = 10, third = 5;
+        double first = 11.11;
+        double expected = 11.11;
+        double output = Max.max(first, second, third);
+        assertThat(output).isEqualTo(expected);
+    }
+
+    @Test
+    void whenFirst10Dot11Second11Dot11Third5Then11Dot11() {
+        int first = 10, third = 5;
+        double  second = 11.11;
+        double expected = 11.11;
+        double output = Max.max(first, second, third);
+        assertThat(output).isEqualTo(expected);
+    }
 }
